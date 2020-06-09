a89ac51a0fb6397f0b598b6737ed72cf2c06c22f
Shubham Dhiman <37249803+dhiman-007@users.noreply.github.com>
2020-06-09 19:24:36 +0530

    included side songs list

--- Java script/app_music_player.js
+++ Java script/app_music_player.js
@@ -1,6 +1,6 @@
+//dMAN
 
-
-let songs = ["Let em play Karan aujla.mp3","Sheikh  Karan Aujla.mp3","East Side Flow - Sidhu Moose Wala.mp3",
+let songs = ["Let em play Karan aujla.mp3", "Sheikh  Karan Aujla.mp3", "East Side Flow - Sidhu Moose Wala.mp3",
     "Sooraj-dooba-hai-remix.mp3",
     "Daru-badnam-remix.mp3",
     "Rock-on-title-song.mp3",
@@ -8,7 +8,7 @@ let songs = ["Let em play Karan aujla.mp3","Sheikh  Karan Aujla.mp3","East Side 
     "I-Like-Me-Better.mp3"
 ];
 
-let pics, sum 
+let pics, sum = "", sng
 
 window.onload = fill
 
@@ -20,55 +20,64 @@ function fill() {
         return "images/Poster" + (++j) + ".jpg"
     })
 
-    document.getElementById('number').innerHTML=1 +' / '+songs.length
+    document.getElementById('number').innerHTML = 1 + ' / ' + songs.length
 
-    // console.log(songs)
-    // for(let i =0 ;i <songs.length;i++){
-    //         sum+='<a href= "">' + songs[i].substring(0, songs[i].length-3) +'</a>' + '<br/><br/>'
-    // }
-    // document.getElementById('main').innerHTML=sum.toString()
-    // console.log(show)
+    sum += '<h3 style="color:cornflowerblue">' + 'Songs(' + songs.length + ')</h3>' + '<br/>'
+
+    for (let i = 0; i < songs.length; i++) {
+        sum += '<br/>' + songs[i].substring(0, songs[i].length - 4) + '<br/><br/>' +
+            '<button class ="beat" id=' + i + '>' + 'Play' + '</button>' +
+            '<br/><br/>'
+    }
+    document.getElementById('list').innerHTML = sum.toString()
 
 }
 
+document.getElementById('list').addEventListener('click', (e) => {
+    if (e.target.tagName == 'BUTTON') {
+        sng = songs[e.target.id]
+        i = (parseInt)(e.target.id)
+         bomb(sng, i + 1)
 
+    }
+
+})
 
 let i = 0;
 
 const pre = () => {
     if (i == 0) {
         i = songs.length - 1;
-        bomb(songs[i], i)
+        bomb(songs[i], i + 1)
     } else {
         i--
-        bomb(songs[i], i)
-
+        bomb(songs[i], i + 1)
     }
 }
 
 const next = () => {
     if (i == songs.length - 1) {
         i = 0;
-        bomb(songs[i], i)
+        bomb(songs[i], i + 1)
 
     }
     else {
         i++;
-        bomb(songs[i], i)
+        bomb(songs[i], i + 1)
 
     }
 }
 
-document.getElementById("my-audio").addEventListener('ended', ()=>{
+document.getElementById("my-audio").addEventListener('ended', () => {
     next()
 })
 
-
 const bomb = (songName, ind) => {
-	console.log(pics)
     document.getElementById("my-audio").setAttribute('src', 'music/' + songName)
     document.getElementById('songTitle').innerHTML = "d'MAN" + '<br/><br/>' + songName.substring(0, songName.length - 4);
-    document.getElementById('number').innerHTML=i+1+' / '+songs.length
-    $("#bg img").attr("src", pics[ind]);
+    document.getElementById('number').innerHTML = (ind) + ' / ' + songs.length
+    $("#bg img").attr("src", pics[ind])
+
 
 }
+
